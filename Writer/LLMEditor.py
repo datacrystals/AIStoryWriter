d70d4d7d422fd90ef22d405635575a44296d54ff
Thomas Liao <tliao@carboncopies.org>
2024-04-25 18:57:13 -0700

    Update System

--- Writer/LLMEditor.py
+++ Writer/LLMEditor.py
@@ -10,10 +10,10 @@ def GetFeedbackOnOutline(_Client, _Outline:str):
     StartingPrompt += "\n\n\n"
     StartingPrompt += _Outline
 
-    Writer.PrintUtils.PrintBanner("Prompting LLM To Critique Outline", "purple")
+    Writer.PrintUtils.PrintBanner("Prompting LLM To Critique Outline", "green")
     Messages = [Writer.OllamaInterface.BuildUserQuery(StartingPrompt)]
     Messages = Writer.OllamaInterface.ChatAndStreamResponse(_Client, Messages)
-    Writer.PrintUtils.PrintBanner("Finished Getting Feedback", "purple")
+    Writer.PrintUtils.PrintBanner("Finished Getting Feedback", "green")
 
     return Writer.OllamaInterface.GetLastMessageText(Messages)
 
@@ -24,10 +24,10 @@ def GetOutlineRating(_Client, _Outline:str):
     StartingPrompt += "\n\n\n"
     StartingPrompt += "Review the above outline honestly, and give a json formatted response, containing the string \"OverallRating\", followed by an integer from 1-100. Please do not include any other text, just the JSON as your response will be parsed by a computer."
 
-    Writer.PrintUtils.PrintBanner("Prompting LLM To Get Review JSON", "purple")
+    Writer.PrintUtils.PrintBanner("Prompting LLM To Get Review JSON", "green")
     Messages = [Writer.OllamaInterface.BuildUserQuery(StartingPrompt)]
     Messages = Writer.OllamaInterface.ChatAndStreamResponse(_Client, Messages)
-    Writer.PrintUtils.PrintBanner("Finished Getting Review JSON", "purple")
+    Writer.PrintUtils.PrintBanner("Finished Getting Review JSON", "green")
 
 
     while True:
@@ -36,7 +36,7 @@ def GetOutlineRating(_Client, _Outline:str):
         
         try:
             Rating = json.loads(RawResponse)["OverallRating"]
-            Writer.PrintUtils.PrintBanner(f"Editor Reviewed Outline At {Rating}/100", "purple")
+            Writer.PrintUtils.PrintBanner(f"Editor Reviewed Outline At {Rating}/100", "green")
             return Rating
         except Exception as E:
             Writer.PrintUtils.PrintBanner("Error Parsing JSON Written By LLM, Asking For Edits", "red")
