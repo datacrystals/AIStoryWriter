715ef2873f615bf35042e0290d2b7c55c16a247b
Thomas Liao <tliao@carboncopies.org>
2024-05-14 19:21:40 -0700

    Add Prompts/Stories to gitignore

--- Writer/LLMEditor.py
+++ Writer/LLMEditor.py
@@ -6,9 +6,12 @@ import json
 
 def GetFeedbackOnOutline(_Client, _Outline:str, _History:list = []):
 
-    StartingPrompt:str = "Please critique the following outline - make sure to provide constructive criticism on how it can be improved and point out any problems with it. Remember to check if the characters' names are spelled correctly."
-    StartingPrompt += "\n\n\n"
-    StartingPrompt += _Outline
+    StartingPrompt:str = f"""
+Please critique the following outline - make sure to provide constructive criticism on how it can be improved and point out any problems with it.
+Remember to check if the characters' names are spelled correctly.
+---
+{_Outline}
+    """
 
     Writer.PrintUtils.PrintBanner("Prompting LLM To Critique Outline", "green")
     Messages = _History
@@ -21,9 +24,14 @@ def GetFeedbackOnOutline(_Client, _Outline:str, _History:list = []):
 
 def GetOutlineRating(_Client, _Outline:str, _History:list = []):
 
-    StartingPrompt:str = _Outline
-    StartingPrompt += "\n\n\n"
-    StartingPrompt += "Review the above outline honestly, and give a json formatted response, containing the string \"OverallRating\", followed by an integer from 1-100. Please do not include any other text, just the JSON as your response will be parsed by a computer."
+    StartingPrompt:str = f"""
+{_Outline}
+
+---
+
+Review the above outline honestly, and give a json formatted response, containing the string \"OverallRating\", followed by an integer from 1-100.
+Please do not include any other text, just the JSON as your response will be parsed by a computer.
+"""
 
     Writer.PrintUtils.PrintBanner("Prompting LLM To Get Review JSON", "green")
     Messages = _History
@@ -84,9 +92,13 @@ Make sure the chapter follows the outline so that the story stays on track!
 
 def GetChapterRating(_Client, _Chapter:str, _History:list = []):
 
-    StartingPrompt:str = _Chapter
-    StartingPrompt += "\n\n\n"
-    StartingPrompt += "Review the above chapter honestly, and give a json formatted response, containing the string \"OverallRating\", followed by an integer from 1-100. Please do not include any other text, just the JSON as your response will be parsed by a computer."
+    StartingPrompt:str = f"""
+{_Chapter}
+
+---
+Review the above chapter honestly, and give a json formatted response, containing the string \"OverallRating\", followed by an integer from 1-100.
+Please do not include any other text, just the JSON as your response will be parsed by a computer.
+"""
 
     Writer.PrintUtils.PrintBanner("Prompting LLM To Get Review JSON", "green")
     Messages = _History
