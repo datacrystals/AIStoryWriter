9e7278e1213c7dc518b86a243f8914602b351535
Thomas Liao <tliao@carboncopies.org>
2024-06-22 01:59:03 -0700

    Add Second Example Prompt

---
+++ Tools/DevTestSmallExample2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd ..
+
+echo "Running tests with smallest and fastest model"
+
+./Write.py \
+-Host localhost:11434 \
+-Seed 999 \
+-Prompt ExamplePrompts/Example2.txt \
+-InitialOutlineModel mistral \
+-ChapterOutlineModel mistral \
+-ChapterS1Model mistral \
+-ChapterS2Model mistral \
+-ChapterS3Model mistral \
+-ChapterS4Model mistral \
+-ChapterRevisionModel mistral \
+-RevisionModel mistral \
+-EvalModel mistral \
+-InfoModel mistral \
+-CheckerModel mistral \
+-NoScrubChapters
