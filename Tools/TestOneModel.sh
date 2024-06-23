08b043b2253465db240f60619cc816b2e3d942d4
Thomas Liao <tliao@carboncopies.org>
2024-06-23 01:18:09 -0700

    Add Single Model Test

---
+++ Tools/TestOneModel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ..
+./Write.py \
+-Host 10.1.65.4:11434 \
+-Seed 999 \
+-Prompt ExamplePrompts/Example1/Prompt.txt \
+-InitialOutlineModel datacrystals/miqulitz120b-v2:latest \
+-ChapterOutlineModel datacrystals/miqulitz120b-v2:latest \
+-ChapterS1Model datacrystals/miqulitz120b-v2:latest \
+-ChapterS2Model datacrystals/miqulitz120b-v2:latest \
+-ChapterS3Model datacrystals/miqulitz120b-v2:latest \
+-ChapterS4Model datacrystals/miqulitz120b-v2:latest \
+-ChapterRevisionModel datacrystals/miqulitz120b-v2:latest \
+-RevisionModel llama3:70b \
+-EvalModel llama3:70b \
+-InfoModel llama3:70b \
+-NoScrubChapters \
+-Debug
+
