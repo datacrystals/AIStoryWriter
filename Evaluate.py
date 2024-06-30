5e94e06fa1cede8760a6c5440cd9262f649f455f
Thomas Liao <tliao@carboncopies.org>
2024-06-30 11:50:14 -0700

    Implement Chapter Extractor

--- Evaluate.py
+++ Evaluate.py
@@ -2,12 +2,59 @@
 
 import argparse
 import time
+import json
 import datetime
 import os
 
 import Writer.Interface.Wrapper
 import Writer.Config
 import Writer.PrintUtils
+
+
+def GetNumChapters(_Client, _Logger, _Story):
+    
+    _Logger.Log("Detecting Num Chapters In Story", 4)
+    Messages = [Interface.BuildSystemQuery("You are a helpful AI language model.")]
+    Messages.append(Interface.BuildUserQuery(f"""
+    I need to identify the number of chapters in the given story:
+                                            
+    <STORY>
+    {Story}
+    </STORY>
+
+    Please respond with JSON as follows:
+    {{
+        "NumChapters": <NumChapters>
+    }}
+
+    Only respond with just JSON - your output will be parsed by a computer.
+    
+    """))
+    Messages = Interface.ChatAndStreamResponse(Logger, Messages, Args.Model)
+    JSON = Interface.GetLastMessageText(Messages)
+
+    _Logger.Log("Finished Detecting Num Chapters In Story", 4)
+
+    return json.loads(JSON)["NumChapters"]
+
+def GetChapter(_Client, _Logger, _Story, _Chapter):
+    
+    _Logger.Log(f"Extracting Chapter {_Chapter} From Story", 4)
+    Messages = [Interface.BuildSystemQuery("You are a helpful AI language model.")]
+    Messages.append(Interface.BuildUserQuery(f"""
+    Please extract chapter {_Chapter} from the following story:
+                                            
+    <STORY>
+    {Story}
+    </STORY>
+
+    Only respond with the full story text for chapter {_Chapter} verbatim (not the outline). Do not include anything else in your response.
+    
+    """))
+    Messages = Interface.ChatAndStreamResponse(Logger, Messages, Args.Model)
+    _Logger.Log(f"Finished Extracting Chapter {_Chapter} From Story", 4)
+
+    return Interface.GetLastMessageText(Messages)
 
 
 # Setup Argparser
@@ -28,12 +75,24 @@ StartTime_s = time.time()
 # Setup Logger
 Logger = Writer.PrintUtils.Logger("EvalLogs")
 
+# Setup Logger
+Interface = Writer.Interface.Wrapper.Interface([Args.Model])
+
 # Load the initial story
 Story:str = ""
 with open(Args.Story, "r") as f:
     Story = f.read()
 
-Interface = Writer.Interface.Wrapper.Interface([Args.Model])
+
+
+ChapterCount = GetNumChapters(Interface, Logger, Story)
+
+Chapters:list = []
+for i in range(ChapterCount):
+    Chapters.append(GetChapter(Interface, Logger, Story, i))
+
+
+
 Messages = [Interface.BuildSystemQuery("You are a helpful AI language model.")]
 Messages.append(Interface.BuildUserQuery(f"""
 Please rate the below story:
