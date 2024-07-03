3949035043ef295ce36f607736a5563b08783da8
LoggeL <hyper.xjo@gmail.com>
2024-07-04 00:53:34 +0200

    chore: Set temperature to 0 for JSON mode in Interface Wrapper

--- Writer/Interface/Wrapper.py
+++ Writer/Interface/Wrapper.py
@@ -200,6 +200,7 @@ class Interface:
                 options=dict(
                     seed=Seed,
                     format=_Format,
+                    # Set temperature (creativity) to 0 for JSON mode otherwise default
                     temperature=0 if _Format == "json" else None,
                 ),
             )
