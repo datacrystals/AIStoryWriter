e6c7c00865fbc644a80598ba69e50410b5c5bb7b
Thomas Liao <tliao@carboncopies.org>
2024-07-12 01:25:34 -0700

    Add Reasons To Text Generation Failure

--- Writer/Interface/Wrapper.py
+++ Writer/Interface/Wrapper.py
@@ -141,7 +141,11 @@ class Interface:
         NewMsg = self.ChatAndStreamResponse(_Logger, _Messages, _Model, _SeedOverride, _Format)
 
         while (self.GetLastMessageText(NewMsg).isspace()) or (len(self.GetLastMessageText(NewMsg).split(" ")) < _MinWordCount):
-            _Logger.Log("Generation Failed, Reattempting Output", 7)
+            if self.GetLastMessageText(NewMsg).isspace():
+                _Logger.Log("SafeGenerateText: Generation Failed Due To Empty (Whitespace) Response, Reattempting Output", 7)
+            elif (len(self.GetLastMessageText(NewMsg).split(" ")) < _MinWordCount):
+                _Logger.Log(f"SafeGenerateText: Generation Failed Due To Short Response ({len(self.GetLastMessageText(NewMsg).split(" "))}, min is {_MinWordCount}), Reattempting Output", 7)
+
             del _Messages[-1] # Remove failed attempt
             NewMsg = self.ChatAndStreamResponse(_Logger, _Messages, _Model, random.randint(0, 99999), _Format)
 
